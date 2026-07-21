dbffe5b27cb7d6ed221974d2ff54fbeb1d2521ff
DK703 <dkim3157@gmail.com>
2026-07-21 18:41:31 +0000

    had it so barita launch file does everything. I honestly prefer the split up method that xml allows, so I kept that.

---
+++ launch/barista_urdf_old.launch.py
@@ -0,0 +1,58 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+# this is the function launch system will look for
+def generate_launch_description():
+
+    ####### DATA INPUT ##########
+    urdf_file = 'barista_robot_model.urdf'
+    #xacro_file = "urdfbot.xacro"
+    package_description = "barista_robot_description"
+
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+
+    rviz_config = os.path.join(
+        get_package_share_directory('barista_robot_description'),
+        'rviz',
+        'file.rviz'  
+    )
+
+    # Joint State Publisher_gui, different from reg joint state publisher
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        output="screen",
+    )
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        output="screen"
+    )
+
+    # create and return launch description object
+    return LaunchDescription(
+        [
+                
+            robot_state_publisher_node,
+            joint_state_publisher_gui,
+            Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', rviz_config],
+            output='screen'
+        ),
+        
+        ]
+    )
