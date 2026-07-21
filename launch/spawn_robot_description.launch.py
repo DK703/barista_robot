a15e25d1f9207029d848f8bbffafeba56e61cacd
DK703 <dkim3157@gmail.com>
2026-07-21 17:40:07 +0000

    fixed the topic name from laser_scan to scan. Currently will try to create one launch file that will set everything up.

--- launch/spawn_robot_description.launch.py
+++ launch/spawn_robot_description.launch.py
@@ -45,6 +45,7 @@ def generate_launch_description():
             "/tf" + "@tf2_msgs/msg/TFMessage" + "[gz.msgs.Pose_V",
             "/odom" + "@nav_msgs/msg/Odometry" + "[gz.msgs.Odometry",
             "/joint_states" + "@sensor_msgs/msg/JointState" + "[gz.msgs.Model",
+            "/scan" + "@sensor_msgs/msg/LaserScan" + "[gz.msgs.LaserScan",
         ],
         output="screen",
     )
